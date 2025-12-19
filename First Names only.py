d3ef09843653d98af6095342a81d21302d76c138
Joe Woodings <Jceswoodings@gmail.com>
2025-12-19 19:27:03 +0000

    added type hints to functions in all files. Rewrote 'First Names Only.py' to be more organised and practice modularisation.

--- First Names only.py
+++ First Names only.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 import time
-def Remove_last_names(name):
+def Remove_last_names(name: str) -> str:
     name = str(name)
     name = name.strip()
     parts = name.split(" ")
@@ -9,25 +9,36 @@ def Remove_last_names(name):
         parts[0] = "Gus"
     return f"{parts[0].capitalize()} {parts[1][0].upper()}" if len(parts) > 1 else parts[0].capitalize()
 
+def Apply_format_to_each(df: pd.DataFrame) -> pd.DataFrame:
+    df2 = {}
+    for col_name, participants in df.items():
+        participants = participants.tolist()
+        value = list(map(Remove_last_names, participants))
+        df2.update({col_name: value})
+    df2 = pd.DataFrame(df2)
+    df2 = df2.fillna(" ")
+    return df2
 
-path_to_use = ""
-while path_to_use == "" or path_to_use == " ":
-    path_to_use = (input("Please paste the file path of the file you want to process here:")
-                   .strip().replace("\"", ""))
-df = pd.read_csv(fr"{path_to_use}")
-df = df.fillna(" ")
-df2 = {}
+def Get_source_path() -> str:
+    source_path = ""
+    while source_path == "" or source_path == " ":
+        source_path = (input("Please paste the file path of the file you want to process here:")
+                    .strip().replace("\"", ""))
+    return source_path  
 
-for col_name, value in df.items():
-    value = value.tolist()
-    value = list(map(Remove_last_names, value))
-    df2.update({col_name: value})
 
-df2 = pd.DataFrame(df2)
-df2 = df2.fillna(" ")
-df2.columns = [" " if "Unnamed: " in course else course for course in df2.columns]
-df2.to_csv(Path.home()/'Desktop'/'First names only allocations GIFT.csv', index=False)
-print("New file created in your desktop, called \'First names only allocations GIFT\'"
-      "\n This window will close after 5 seconds")
-time.sleep(5)
+def main():
+    source_path = Get_source_path()
+
+    df = pd.read_csv(fr"{source_path}")
+    df = df.fillna(" ")
+    df2 = Apply_format_to_each(df)
+    df2.columns = [" " if "Unnamed: " in course else course for course in df2.columns]
+    df2.to_csv(Path.home()/'Desktop'/'First names only allocations GIFT.csv', index=False)
+    print("New file created in your desktop, called \'First names only allocations GIFT\'"
+          "\n This window will close after 5 seconds")
+    time.sleep(5)
+    
+if __name__ == "__main__":
+    main()
 
