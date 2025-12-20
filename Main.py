2ce124d9b18b0fa20b491a67c3dfd6a13e64448e
Joe Woodings <Jceswoodings@gmail.com>
2025-12-20 17:52:19 +0000

    .gitignore now includes all __pycache__ files. Improved comments in Classes.py and removed redundant classes from Main.py.

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import sys
 import time as t
 
 from JSON import load_config
+from Classes import Student, Course
 
 config = load_config('Config.json')
 
@@ -61,37 +62,6 @@ def avg_sat_score(students: list) -> float:
         student_count += 1
     mean_score = round(float(mean_score / student_count), 3)
     return mean_score
-
-
-class Student:
-
-    def __init__(self, info: dict) -> None:
-        try:
-            self.name = info.pop("Student Name")
-            self.name = self.name[0].upper() + self.name[1:]
-            self.assigned_courses = set()
-            self.satisfaction_score = 0
-
-            processed_info = {}  # dictionary with seperators removed
-            for key, value in info.items():
-                if "_" in key or "_" in value:
-                    pass
-                else:
-                    processed_info.update({key: value})
-            # .pop() removes the specified item (or last if blank), and returns the value removed
-            self.preferences = {key: int(value) for key, value in processed_info.items()}
-            # info is a dictionary  {course_name : preference}
-        except ValueError:
-            pass
-
-
-class Course:
-
-    def __init__(self, name: str, max_students: int) -> None:
-        self.name = name
-        self.max_students = max_students
-        self.assigned_students = []
-        self.student_count = 0
 
 
 # =====================================================================================================================
