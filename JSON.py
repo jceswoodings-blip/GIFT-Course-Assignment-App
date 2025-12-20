dd340f1460cb2fa62c8df14f1b9ff0296dc07af2
Joe Woodings <Jceswoodings@gmail.com>
2025-12-20 18:57:19 +0000

    added docstring to load_config in 'JSON.py', added 'data_loader.py'.

--- JSON.py
+++ JSON.py
@@ -1,5 +1,13 @@
 import json
 def load_config(path: str) -> dict:
+    """
+    Load configuration settings from a JSON file.
+    Args:
+        path (str): The file path for the JSON file you want to load.
+    Returns:
+        config (dict): The JSON as a dictionary.
+
+    """
     with open(path, 'r') as file:
         config = json.load(file)
     return config
